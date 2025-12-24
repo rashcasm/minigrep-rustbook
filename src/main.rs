dcdef5ae3ae7671c0f64d74c5a85e0c8244f17fa
rashmin <rashmin9c@gmail.com>
2025-12-24 20:32:36 +0530

    added iterators now

--- src/main.rs
+++ src/main.rs
@@ -4,27 +4,18 @@ use std::error::Error;
 use minigrep::search;
 
 fn main() {
-    // part 1 - accepting command line arguments
-    let args: Vec<String> = env::args().collect();
-    let config = Config::build(&args).unwrap_or_else(|err|{
-        eprintln!("Error message: {err}");
+    let config = Config::build(env::args()).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {err}");
         std::process::exit(1);
     });
 
     println!("Searching for {}", config.query);
     println!("In file {}", config.file_path);
 
-    // part 2 - reading a file
-    // let contents = fs::read_to_string(file_path)
-    //     .expect("Should have been able to read the file");
-
-    // println!("With text:\n{contents}");
-    
     if let Err(e) = run(config) {
-        println!("Application error: {e}");
+        eprintln!("Application error: {e}");
         std::process::exit(1);
     }
-
 }
 
 struct Config {
@@ -33,17 +24,29 @@ struct Config {
 }
 
 impl Config {
-    fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
+    fn build(
+        mut args: impl Iterator<Item = String>,
+    ) -> Result<Config, &'static str> {
+        args.next();
 
-        let query = args[1].clone();
-        let file_path = args[2].clone();
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a query string"),
+        };
 
-        Ok(Config { query, file_path })
+        let file_path = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a file path"),
+        };
+
+
+        Ok(Config {
+            query,
+            file_path
+        })
     }
 }
+
 
 fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.file_path)?;
