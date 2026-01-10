02545f1fbab3fddcce8c0e08dbdb000da58ec9b2
rashmin <rashmin9c@gmail.com>
2026-01-10 21:58:12 +0530

    better lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,11 @@
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     let query = query.to_lowercase();
+
     contents
         .lines()
         .filter(|line| line.to_lowercase().contains(&query))
         .collect()
 }
-
 
 #[cfg(test)]
 mod tests {
@@ -20,6 +20,14 @@ safe, fast, productive.
 Pick three.
 Duct tape.";
 
-        assert_eq!(vec!["safe, fast, productive.", "Duct tape."], search(query, contents));
+        let result = search(query, contents);
+
+        assert_eq!(
+            vec![
+                "safe, fast, productive.",
+                "Duct tape."
+            ],
+            result
+        );
     }
 }
